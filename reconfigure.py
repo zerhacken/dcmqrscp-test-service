192ca8ad086c6be18bfacb531e6926b0dd3bbd00
Rasmus Christian Pedersen <zerhacken@yahoo.com>
2017-03-21 23:12:05 +0100

    dcmqrscp-test-service: reconfigure.py with storescu of local .dcm files
    WIP: started working on support for movescu of all files stored in
    sample folder 'after' start of dcmqrscp.

--- reconfigure.py
+++ reconfigure.py
@@ -3,11 +3,11 @@ import zipfile
 import tarfile
 import subprocess
 import sys
+import os
+import time
 
-# sys.platform == "win32"
 filename_osx = "dcmtk-3.6.0-mac-i686-static.tar.bz2"
 filename_win32 = "dcmtk-3.6.0-win32-i386.zip"
-
 url = "ftp://dicom.offis.de/pub/dicom/offis/software/dcmtk/dcmtk360/bin/"
 
 with open(filename_win32 if sys.platform == "win32" else filename_osx ,'wb') as f:
@@ -17,14 +17,30 @@ with open(filename_win32 if sys.platform == "win32" else filename_osx ,'wb') as 
 if sys.platform == "win32":
     with zipfile.ZipFile(filename_win32, "r") as z:
         z.extractall("temp")
+        os.remove(filename_win32)
 else:
     with tarfile.open(filename_osx) as tar:
         tar.extractall("temp")
+        os.remove(filename_osx)
 
 # dcmqrscp --log-level trace --config db/dcmqrscp.cfg
 # win32 temp/dcmtk-3.6.0-win32-i386/bin
 # macOS temp/dcmtk-3.6.0-mac-i686-dynamic/bin
 if sys.platform == "win32":
-    subprocess.call(["temp/dcmtk-3.6.0-win32-i386/bin/dcmqrscp --log-level trace --config db/dcmqrscp.cfg"], shell=True)
+    subprocess.call(["temp/dcmtk-3.6.0-win32-i386/bin/dcmqrscp --log-level trace --config db/dcmqrscp.cfg"],
+        shell=True, stdin=None, stdout=None, stderr=None)
 else:
-	subprocess.call(["temp/dcmtk-3.6.0-mac-i686-dynamic/bin/dcmqrscp --log-level trace --config db/dcmqrscp.cfg"], shell=True)
+    subprocess.Popen(["temp/dcmtk-3.6.0-mac-i686-dynamic/bin/dcmqrscp --log-level trace --config db/dcmqrscp.cfg"],
+        shell=True)
+
+# HACKME:
+time.sleep(10.0)
+local = "temp/dcmtk-3.6.0-mac-i686-dynamic/bin/"
+# storescu -v --aetitle SCP --call STORAGE localhost 5678 sample/mono2.dcm
+for dcm_file in os.listdir("sample"):
+    subprocess.Popen([local + "storescu -v --aetitle SCP --call STORAGE localhost 5678 sample/" + dcm_file],
+        shell=True)
+
+
+
+
